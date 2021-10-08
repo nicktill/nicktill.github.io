79f7d7bcdc066d22b7e49cb6573bc2ab867c3cb1
Nicholas Tillmann <nit36@pitt.edu>
2021-10-08 15:36:30 -0400

    adding navbar

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+import react from 'react';
+
+export const MenuItems = [
+    {
+        title: "Home",
+        url: '#',
+        cName: 'nav-links',
+    },
+
+    {
+        title: "LinkedIn",
+        url: '#',
+        cName: 'nav-links',
+    },
+
+    {
+        title: "GitHub",
+        url: '#',
+        cName: 'nav-links',
+    },
+
+    {
+        title: "Resume",
+        url: '#',
+        cName: 'nav-links',
+    }
+]
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { MenuItems } from './MenuItems';
+
+class Navbar extends Component {
+    render() {
+        return (
+            <nav className="NavbarItems">
+                <h1 className="navbar-logo">React</h1>
+                <div className="menu-icon">
+                </div>
+                <ul>
+                    <li><a href=""></a></li>
+                </ul>
+
+
+            </nav>
+
+        )
+    }
+}

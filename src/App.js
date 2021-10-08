4e59bbb560ae4dea41332ea7459ab93126df965d
Nicholas Tillmann <nit36@pitt.edu>
2021-10-08 13:46:54 -0400

    added links to linkedin github

--- src/App.js
+++ src/App.js
@@ -5,9 +5,17 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>nicholas-tillmann</h1>
+        <h1>nicholas tillmann</h1>
         <h2>personal website</h2>
+        <a href="https://www.linkedin.com/in/nicholas-tillmann-4647b7187"><span>LinkedIn</span></a>
+        <a href="https://github.com/nicktill"><span>Github</span></a>
+
       </header>
+      <div class="wrapper">
+        <div class="typing-demo">
+          website coming soon
+        </div>
+      </div>
     </div>
   );
 }

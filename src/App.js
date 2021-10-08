f37e21c73f69b2411330d9539c9335ee97a9a07a
unknown <ntillmann1439@gmail.com>
2021-10-08 16:57:29 -0400

    update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
         <h1>nicholas tillmann</h1>
         <h2>personal website</h2>
         <a href="https://www.linkedin.com/in/nicholas-tillmann-4647b7187" class="links"><span>LinkedIn</span></a><br></br>
-        <a href="https://github.com/nicktill" class="links"><span>Github</span></a>
+        <a href="https://github.com/nicktill" class="links"><span>Github</span></a><br></br>
+        <a href="\Users\Nick\Desktop\newWebsite\src\resume.pdf" class="links">Resume</a><br></br>
+        <a href = "mailto: ntillmann1439@gmail.com" class="links">Contact</a>
       </header>
       <div class="wrapperText wrapper">
         <div class="typing-demo">

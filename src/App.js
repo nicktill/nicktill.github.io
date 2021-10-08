4d99fcfb5407491953268f282a83276e5fea2f64
unknown <ntillmann1439@gmail.com>
2021-10-08 16:24:39 -0400

    adding adaptability to browsers so that elements do not move when viewing on different dimensions

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header>
+      <header class = "wrapper">
         <h1>nicholas tillmann</h1>
         <h2>personal website</h2>
         <a href="https://www.linkedin.com/in/nicholas-tillmann-4647b7187" class="links"><span>LinkedIn</span></a><br></br>
         <a href="https://github.com/nicktill" class="links"><span>Github</span></a>
       </header>
-      <div class="wrapper">
+      <div class="wrapperText wrapper">
         <div class="typing-demo">
           website coming soon
         </div>

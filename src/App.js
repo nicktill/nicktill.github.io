8dcfb20890de95542a74b56fe6b1768c28dcbcce
Nicholas Tillmann <nit36@pitt.edu>
2021-10-08 13:50:03 -0400

    fixed spacing

--- src/App.js
+++ src/App.js
@@ -7,9 +7,8 @@ function App() {
       <header>
         <h1>nicholas tillmann</h1>
         <h2>personal website</h2>
-        <a href="https://www.linkedin.com/in/nicholas-tillmann-4647b7187"><span>LinkedIn</span></a>
-        <a href="https://github.com/nicktill"><span>Github</span></a>
-
+        <a href="https://www.linkedin.com/in/nicholas-tillmann-4647b7187" class="links"><span>LinkedIn</span></a><br></br>
+        <a href="https://github.com/nicktill" class="links"><span>Github</span></a>
       </header>
       <div class="wrapper">
         <div class="typing-demo">

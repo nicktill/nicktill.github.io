23bb1c1f0eba4bccd153f6e8f1b978028c561e89
Nicholas Tillmann <nit36@pitt.edu>
2021-10-08 11:15:32 -0400

    add header

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>temporary</h1>
+      <header>
+        <h1>Nick's Todo List</h1>
+      </header>
+
     </div>
   );
 }

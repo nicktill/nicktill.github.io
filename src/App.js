35a42f35e726472ff1c71beef5a038af5b0f59c7
Nicholas Tillmann <nit36@pitt.edu>
2021-10-09 02:27:23 -0400

    update

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ function App() {
     <div className="App">
       <NavBar />
       <header class="wrapper">
-        <h1 class="opacity">nicholas_tillmann</h1>
+        <h1 class="opacity">nicholas tillmann</h1>
         <h5 class="opacity-two">SWE Intern @Leidos | CS @Pitt</h5>
       </header>
       <div class="wrapperText wrapper">
         <div class="typing-demo opacity-two">
-          new features coming soon...
+          new features soon...
         </div>
       </div>
     </div>

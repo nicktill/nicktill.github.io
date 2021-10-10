8a193a0c5af656daaf722bd8fe7f68d9545c9ba3
Nicholas Tillmann <nit36@pitt.edu>
2021-10-10 03:17:05 -0400

    added highlight features for ul navbar elements

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,19 +7,9 @@ class Navbar extends Component {
     //initialize state to false
     state = { clicked: false }
 
-    //handleClick - if clicked, have it to the opposite of what it is currently
-    handleClick = () => {
-        this.setState({ clicked: !this.state.clicked })
-    }
-
     render() {
         return (
             <nav className="NavbarItems">
-                <div className="menu-icon" onClick={this.handleClick}>
-                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
-                </div>
-
-                {/* this creates the nav bar menu at the top with all the values  */}
                 <ul>
                     {MenuItems.map((item, index) => {
                         return (

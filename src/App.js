1eaaf54e02dc2ee1595e195ce7caaab39a8584a1
Nicholas Tillmann <nit36@pitt.edu>
2021-10-12 02:27:39 -0400

    adding cool header for name

--- src/App.js
+++ src/App.js
@@ -19,16 +19,19 @@ function App() {
       />
       {/* <Jobcard /> */}
       <NavBar />
-      <header class="wrapper">
-        <h1>nicholas tillmann</h1>
-        <h5>SWE Intern @Leidos | CS @Pitt</h5>
-      </header>
-      <div class="wrapper">
-        <div class="typing-demo">
-          new features soon...
+
+
+      <header>
+        <div class="positionContent">
+          <h1 class="coolAnimation small">nicktill</h1>
+          <div positionContentInfo></div>
+          <h5>SWE Intern @Leidos | CS @Pitt</h5>
+          <div class="typing-demo">
+            new features soon...
+          </div>
         </div>
-      </div>
-    </div>
+      </header>
+    </div >
   );
 }
 export default App;

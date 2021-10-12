92fc8e82771f411ee18a64b80b695dbea036491f
Nicholas Tillmann <nit36@pitt.edu>
2021-10-12 02:33:20 -0400

    adding name field

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
       <header>
         <div class="positionContent">
           <h1 class="coolAnimation small">nicktill</h1>
+          <h3>nicholas tillmann</h3>
           <div positionContentInfo></div>
           <h4>SWE Intern @Leidos | CS @Pitt</h4>
           <div class="typing-demo">

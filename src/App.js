5472f7f89bf7ee77c0f461b2d9c21a402fa33beb
Nicholas Tillmann <nit36@pitt.edu>
2021-10-12 02:30:50 -0400

    updating margin, padding, font sizes

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
         <div class="positionContent">
           <h1 class="coolAnimation small">nicktill</h1>
           <div positionContentInfo></div>
-          <h5>SWE Intern @Leidos | CS @Pitt</h5>
+          <h4>SWE Intern @Leidos | CS @Pitt</h4>
           <div class="typing-demo">
             new features soon...
           </div>

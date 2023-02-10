528aaf20174d7c51614dd1417645ee92291f24ae
Nicholas Tillmann <ntillmann1439@gmail.com>
2023-02-10 11:27:24 -0500

    mobile friendly

--- src/App.js
+++ src/App.js
@@ -1,35 +1,23 @@
-import React from 'react';
-import './App.css';
-import NavBar from "./components/Navbar/Navbar"
-import AnimatedCursor from "react-animated-cursor"
+import React from "react";
+import "./App.css";
+import NavBar from "./components/Navbar/Navbar";
+import AnimatedCursor from "react-animated-cursor";
 // import Jobcard from "./components/Jobcard/Jobcard"
 // import morphCard from './components/morphCard/morphCard';
 
-let emailHandler = 'new features soon...';
-
-const copyEmail = () => {
-  // // add code to copy email
-  // var link = "ntillmann1439@gmail.com"
-  //   + "?cc=myCCaddress@example.com"
-  //   + "&subject=" + encodeURIComponent("")
-  //   + "&body=" + encodeURIComponent(document.getElementById('').value)
-  //   ;
-
-  // window.location.href = link;
-}
 function App() {
   return (
     <div>
       <AnimatedCursor
         innerSize={8}
         outerSize={24}
-        color='255,255,255'
+        color="255,255,255"
         outerAlpha={0}
         innerScale={0.7}
         outerScale={5}
         trailingSpeed={0}
       />
-      <NavBar/>
+      <NavBar />
       <header>
         <div class="positionContent">
           <h1 class="coolAnimation small">nicktill</h1>
@@ -37,7 +25,7 @@ function App() {
           <h4>CS @Pitt</h4>
         </div>
       </header>
-    </div >
+    </div>
   );
 }
 

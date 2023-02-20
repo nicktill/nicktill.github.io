517b5d2b46e4c1814b0a16a8b69438f2327670ee
Nicholas Tillmann <ntillmann1439@gmail.com>
2023-02-20 13:39:11 -0500

    adding info component, typing effect, and background img

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Info from "./components/Info";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Info />
     </div>
   );
 }

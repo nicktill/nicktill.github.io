f2fc33a68a73f1ec1c7aa84aca4436ce064ab145
Nicholas Tillmann <ntillmann1439@gmail.com>
2023-02-21 10:27:14 -0500

    adding new header img

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import headerImg from "../assets/img/header-img3.svg";
+import headerImg from "../assets/img/header-astro.png";
 import "animate.css";
 import TrackVisibility from "react-on-screen";
 import React from "react";

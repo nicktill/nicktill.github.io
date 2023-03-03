aa51c7840990ca7bdbab9dea99c9067772c469ea
Nicholas Tillmann <ntillmann1439@gmail.com>
2023-03-02 23:17:56 -0500

    changing info

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -80,8 +80,8 @@ export const Banner = () => {
                       </span>
                     </h1>
                     <p>
-                      I love creating beautiful and intuitive interfaces that
-                      make users' lives easier
+                      I create beautiful and intuitive interfaces that make
+                      users' lives easier
                     </p>
                   </div>
                 )}

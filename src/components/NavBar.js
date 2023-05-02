893b0029d0afca2697509d0e295e95ef38b5a9c6
Nicholas Tillmann <ntillmann1439@gmail.com>
2023-05-02 18:34:28 -0400

    update resume

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -33,26 +33,7 @@ export const NavBar = () => {
           <span className="navbar-toggler-icon"></span>
         </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link
-              href="#home"
-              className={
-                activeLink === "home" ? "active navbar-link" : "navbar-link"
-              }
-              onClick={() => onUpdateActiveLink("home")}
-            >
-              Home
-            </Nav.Link>
-            <Nav.Link
-              href="#projects"
-              className={
-                activeLink === "projects" ? "active navbar-link" : "navbar-link"
-              }
-              onClick={() => onUpdateActiveLink("projects")}
-            >
-              Projects
-            </Nav.Link>
-          </Nav>
+          <Nav className="ms-auto"></Nav>
           <span className="navbar-text">
             <div className="social-icon">
               <a href="https://nicktill.github.io/resume.pdf" target="_blank">

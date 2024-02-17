3c021c4bc19c3859978733c67d5e952c4671d0f4
Nicholas Tillmann <ntillmann1439@gmail.com>
2024-02-17 16:43:34 -0500

    Updates

---
+++ precache-manifest.824a5f6484adcd72abfd20841221a3e7.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "e84468af28f1f06454ce",
+    "url": "/static/css/main.0cc65456.chunk.css"
+  },
+  {
+    "revision": "e84468af28f1f06454ce",
+    "url": "/static/js/main.2d5d61bb.chunk.js"
+  },
+  {
+    "revision": "bcf2ae1b497a127ac466",
+    "url": "/static/js/runtime~main.f177652b.js"
+  },
+  {
+    "revision": "ee39615aa1aa9c13a7fe",
+    "url": "/static/css/2.3bf87e9a.chunk.css"
+  },
+  {
+    "revision": "ee39615aa1aa9c13a7fe",
+    "url": "/static/js/2.7db5857e.chunk.js"
+  },
+  {
+    "revision": "9a4fd47e081488c3852b",
+    "url": "/static/js/3.f175f6c1.chunk.js"
+  },
+  {
+    "revision": "0eb2524e588d48024f64ab24dac9959d",
+    "url": "/static/media/header-astro.0eb2524e.png"
+  },
+  {
+    "revision": "0d22018f01916bdf0e13d5ec5d526594",
+    "url": "/static/media/planet2.0d22018f.png"
+  },
+  {
+    "revision": "96805cac83c7108442ee60d4dd0cf2fa",
+    "url": "/static/media/GradeMyAid.96805cac.png"
+  },
+  {
+    "revision": "cecb897a17642179163aa506cdd599e9",
+    "url": "/static/media/CryptoTracker.cecb897a.png"
+  },
+  {
+    "revision": "b224c3d849f3e40d9519f8ffcee75f8f",
+    "url": "/static/media/ytlight.b224c3d8.png"
+  },
+  {
+    "revision": "539ee8ebe52572775b4b6bfdfef4125e",
+    "url": "/static/media/translator.539ee8eb.png"
+  },
+  {
+    "revision": "897ee1bd7b2be40c3f261bc901f869b5",
+    "url": "/static/media/portfolio.897ee1bd.png"
+  },
+  {
+    "revision": "386f05946a76935fcabf319affff517a",
+    "url": "/static/media/CentraNo2-Book.386f0594.ttf"
+  },
+  {
+    "revision": "63911e8535278ced52b8a37e819f6f5d",
+    "url": "/static/media/CentraNo2-Medium.63911e85.ttf"
+  },
+  {
+    "revision": "4ea3794d631bedab7999678f46b0a162",
+    "url": "/static/media/CentraNo2-Bold.4ea3794d.ttf"
+  },
+  {
+    "revision": "5f3ca49706055342e92708c7dd31849f",
+    "url": "/static/media/banner-bg.5f3ca497.png"
+  },
+  {
+    "revision": "029b2907d1ee6aeb1e7c112243f577c0",
+    "url": "/index.html"
+  }
+];

b9446a7b9074073717b5ec0b1a37c9ca4bc85215
Nick TIllmann <ntillmann1439@gmail.com>
2025-05-29 14:51:28 -0400

    Updates

---
+++ precache-manifest.52affd6ee809cc59bda6d10911a58140.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb36aa97594e19805902",
+    "url": "/static/css/main.e88e9c12.chunk.css"
+  },
+  {
+    "revision": "eb36aa97594e19805902",
+    "url": "/static/js/main.eb36aa97.chunk.js"
+  },
+  {
+    "revision": "4388f90dacfdfcfacb8c",
+    "url": "/static/js/1.4388f90d.chunk.js"
+  },
+  {
+    "revision": "72fe3de5a69530f9cab3",
+    "url": "/static/css/2.5fb15b0d.chunk.css"
+  },
+  {
+    "revision": "72fe3de5a69530f9cab3",
+    "url": "/static/js/2.72fe3de5.chunk.js"
+  },
+  {
+    "revision": "091769fa5b92c2251ec0",
+    "url": "/static/js/runtime~main.091769fa.js"
+  },
+  {
+    "revision": "96805cac83c7108442ee60d4dd0cf2fa",
+    "url": "/static/media/GradeMyAid.96805cac.png"
+  },
+  {
+    "revision": "cecb897a17642179163aa506cdd599e9",
+    "url": "/static/media/CryptoTracker.cecb897a.png"
+  },
+  {
+    "revision": "b224c3d849f3e40d9519f8ffcee75f8f",
+    "url": "/static/media/ytlight.b224c3d8.png"
+  },
+  {
+    "revision": "2b08da359412975cb4ba0880644d5632",
+    "url": "/static/media/StockPulse.2b08da35.png"
+  },
+  {
+    "revision": "2a49e373f22e7f195e0d32f5668087a2",
+    "url": "/static/media/clarity-coach.2a49e373.png"
+  },
+  {
+    "revision": "4ea3794d631bedab7999678f46b0a162",
+    "url": "/static/media/CentraNo2-Bold.4ea3794d.ttf"
+  },
+  {
+    "revision": "386f05946a76935fcabf319affff517a",
+    "url": "/static/media/CentraNo2-Book.386f0594.ttf"
+  },
+  {
+    "revision": "63911e8535278ced52b8a37e819f6f5d",
+    "url": "/static/media/CentraNo2-Medium.63911e85.ttf"
+  },
+  {
+    "revision": "b9812e665ea4002b980c881b906b569c",
+    "url": "/static/media/banner-2.b9812e66.jpg"
+  },
+  {
+    "revision": "0eb2524e588d48024f64ab24dac9959d",
+    "url": "/static/media/header-astro.0eb2524e.png"
+  },
+  {
+    "revision": "0d22018f01916bdf0e13d5ec5d526594",
+    "url": "/static/media/planet2.0d22018f.png"
+  },
+  {
+    "revision": "539ee8ebe52572775b4b6bfdfef4125e",
+    "url": "/static/media/translator.539ee8eb.png"
+  },
+  {
+    "revision": "f2d6d9f4abb9daa8b9decff840e14768",
+    "url": "/index.html"
+  }
+];
